a8a717f19addfa4a6b2ac8891b122cffbf109e6c
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-10-09 12:25:57 -0500

    complete and test passed got rid of unused code

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,10 +11,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -36,7 +36,7 @@ explored = {}  # create an empty dictionary to save all explored rooms
 backtrack = []  # empty list to save backtracking path, in order to back track when needed
 
 # all possible directions saved as keys, and exit (opposite direction) saved as values direction:opposite
-direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+move_direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
 # Save Room Number (current_room) in explored as a key, with each possible exit direction as values
 explored[player.current_room.id] = player.current_room.get_exits()
@@ -47,20 +47,34 @@ while len(explored) < len(room_graph):  # while all the rooms hav not been explo
         explored[player.current_room.id] = player.current_room.get_exits()
         # previous room is saved as last direction in backtracking path
         prevRoom = backtrack[-1]
-        print('Previous Room: ', prevRoom, 'Backtracking Path: ', backtrack, 'Current Room of Player: ',
-              player.current_room.id)  # print previous room, backtracking path and current room of player
+       # print('Previous Room: ', prevRoom, 'Backtracking Path: ', backtrack, 'Current Room of Player: ',
+              #player.current_room.id)  # print previous room, backtracking path and current room of player
         # remove direction coming from as possible exit
         explored[player.current_room.id].remove(prevRoom)
     # otherwise if the current room has no unused exits, pick one and check it off
     elif len(explored[player.current_room.id]) > 0:
         # nextRoom = the last of current_room exits
         nextRoom = explored[player.current_room.id][-1]
-        print('Next Room: ', nextRoom, 'Backtracking Path: ', backtrack,
-              'Current Room of Player: ', player.current_room.id)
-        backtrack.pop()  # remove it from the backtracking path
-        traversal_path.append(prevRoom)  # add previous room to traversal path
-        player.travel(prevRoom)  # go back to previous room
-        print('Going back to: ', prevRoom)
+       # print('Next Room: ', nextRoom, 'Backtracking Path: ', backtrack,
+             # 'Current Room of Player: ', player.current_room.id)
+        # remove it from the explored path
+        explored[player.current_room.id].pop()
+        traversal_path.append(nextRoom)  # add next room to traversal path
+        # add the move to backtracking path
+        backtrack.append(move_direction[nextRoom])
+        player.travel(nextRoom)  # go to next room
+        #print('Going to the: ', nextRoom)
+    # if current room has no exits, go back
+    elif len(explored[player.current_room.id]) == 0:
+        # previous room is saved as last direction in backtracking path
+        prevRoom = backtrack[-1]
+        #print('Previous Room: ', prevRoom, 'Backtracking Path: ', backtrack, 'Current Room of Player: ',
+              #player.current_room.id)
+        backtrack.pop()  # remove the direction from backtracking path
+        # add the previous room to the traversal path
+        traversal_path.append(prevRoom)
+        player.travel(prevRoom)  # go back to the previous room
+        #print('Going back to: ', prevRoom)
 
 
 # TRAVERSAL TEST
